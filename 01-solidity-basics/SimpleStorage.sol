0a7d4192e7742f0876575c2662ff93a29997487b
max-wire <maxwellwire454@gmail.com>
2025-11-26 11:51:53 +0300

    Add SimpleStorage contract for Solidity basics

---
+++ 01-solidity-basics/SimpleStorage.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.24; // stating our version
+
+
+/**
+ * @title SimpleStorage
+ * @author Maxwell
+ * @notice Stores and retrieves a favorite number
+ * @dev Learning focus: state variables and functions
+ */
+
+contract SimpleStorage {
+     uint256 myfavoriteNumber; 
+
+     function store(uint256 _favoriteNumber) public virtual {
+        myfavoriteNumber = _favoriteNumber;
+        
+     }
+    
+     function retrieve() public view returns(uint256){
+        return myfavoriteNumber;
+     }
+      
+  
+}
